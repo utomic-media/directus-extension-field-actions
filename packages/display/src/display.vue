15b4c7be99612899078be2b78023cbbec4a5acd7
Dominic <domi.m.98@googlemail.com>
2022-08-08 19:25:35 +0200

    Feat: Add display with it's actions

--- packages/display/src/display.vue
+++ packages/display/src/display.vue
@@ -1,16 +1,152 @@
 <template>
-	<div>Value: {{ value }}</div>
+	<value-null v-if="!value" />
+	<div v-else class="display-action-display">
+		<v-hover v-slot="{ hover }" class="hover-test">
+
+			<component
+				:is="(clickAction === 'link') ? 'a' : 'span'" 
+				class="dynamic-wrapper"
+				:href="computedLink"
+			>
+				<span 
+					:class="hasValueClickAction ? 'action-background' : ''"
+					@click="valueClickAction"
+				>
+					{{ value }}
+				</span>
+			</component>
+			
+
+			<v-icon 
+				v-if="showCopy && isCopySupported"
+				name="content_copy"
+				right
+				@click.stop="copyValue"
+				v-tooltip="`Copy to clipboard: ${value}`"
+			/>
+			
+
+			<a 
+				v-if="showLink"
+				:href="computedLink"
+				target="_blank"
+				rel="noopener noreferrer"
+				@click.stop
+			>
+				<v-icon 
+					name="open_in_new"
+					right
+				/>
+			</a>
+
+		</v-hover>
+	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-	props: {
-		value: {
-			type: String,
-			default: null,
-		},
+
+
+<script setup lang="ts">
+import {computed } from 'vue';
+import { useClipboard } from '../../../shared/composable/use-clipboard';
+import { useStores } from '@directus/extensions-sdk';
+
+const props = defineProps({
+	value: {
+		type: String,
+		default: null,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	interfaceOptions: {
+		type: Object,
+		default: {},	// TODO: type to options!
+	},
+	contentType: {
+		type: String,
+		default: 'other',
+	},
+	clickAction: {
+		type: String,
+		default: 'default',
+	},
+	showCopy: {
+		type: Boolean,
+		default: false,
+	},
+	showLink: {
+		type: Boolean,
+		default: false,
 	},
 });
+
+
+const { isCopySupported, copyToClipboard } = useClipboard();
+
+
+const { useNotificationsStore } = useStores();
+const notificationStore = useNotificationsStore();	
+
+
+async function copyValue() {
+	await copyToClipboard(props.value, notificationStore);
+};
+
+
+function valueClickAction(e: Event) {
+	if (props.clickAction === 'copy') {
+		e.stopPropagation();
+		copyValue();
+	} 
+	// else go on with the default events
+}
+
+
+const hasValueClickAction = computed(() => {
+	if (props.clickAction === 'default') return false;
+	if (props.clickAction === 'copy' && isCopySupported) return true;
+	if (props.clickAction === 'link') return true;
+
+	return false;
+});
+
+
+const computedLink = computed(() => {
+	// TODO: compute web, mail or phone link
+	if (props.contentType === 'url') { return props.value}	// TODO: make sure it's really a link 
+	if (props.contentType === 'email') { return `mailto:${props.value}`}
+	if (props.contentType === 'phone') { return `tel:${props.value}`}
+
+	return props.value;
+});
+
 </script>
+
+
+
+
+<style lang="scss" scoped>
+	.display-action-display {
+		:deep(.v-icon) {
+			--v-icon-size: 18px;
+			--v-icon-color: var(--border-normal-alt);
+
+			&:hover {
+				--v-icon-color: var(--primary);
+			}
+		}
+
+		.action-background {
+			background-color: var(--primary-10);
+			padding: 0.5rem 1rem;
+    	border-radius: 5rem; // aritary value for a nice smooth rounding
+
+			&:hover {
+				background-color: var(--primary-25);
+			}
+		}
+	}
+	
+</style>
--- packages/display/src/index.ts
+++ packages/display/src/index.ts
@@ -1,4 +1,5 @@
-import { defineDisplay } from '@directus/extensions-sdk';
+import { defineDisplay, } from '@directus/extensions-sdk';
+import { DisplayConfig } from '@directus/shared/types';
 import DisplayComponent from './display.vue';
 
 export default defineDisplay({
@@ -7,6 +8,105 @@ export default defineDisplay({
 	icon: 'ads_click',
 	description: 'Display content with actions line linking or copy to clipboard. (By clicking on the content (only at readonly) and seperate buttons)!',
 	component: DisplayComponent,
-	options: null,
-	types: ['string', 'text', 'bigInteger', 'integer'],
+	types: ['string', 'text', 'bigInteger', 'integer'],	// TODO: add decimal and float support!
+	options: ({ field }): any => {
+		const isString = ['string', 'text'].includes(field.type ?? 'unknown');
+		
+		
+		const options: DisplayConfig['options'] = [
+			// TODO: add other choices than 'other' only when isString===true, or phone if is numeric
+			{
+				field: 'contentType',
+				name: 'Fields content type',
+				type: 'string',
+				meta: {
+					width: 'full',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{
+								text: 'Other',
+								value: 'other',
+							},
+							{
+								text: 'URL',
+								value: 'url',
+							},{
+								text: 'Phone',
+								value: 'phone',
+							},
+							{
+								text: 'E-Mail',
+								value: 'email',
+							},
+						],
+					}
+				},
+				schema: {
+					default_value: 'other',
+				},
+			},
+			// TODO: allow "link" only if is string!
+			{
+				field: 'clickAction',
+				name: 'Click Action (when clicking on the value)',
+				type: 'string',
+				meta: {
+					width: 'full',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{
+								text: 'Open element (default)',
+								value: 'default',
+							},
+							{
+								text: 'Open link',
+								value: 'link',
+							},{
+								text: 'Copy to clipboard',
+								value: 'copy',
+							},
+						],
+					}
+				},
+				schema: {
+					default_value: 'default',
+				},
+			},
+			{
+				field: 'showCopy',
+				name: 'Display copy icon',
+				type: 'boolean',
+				meta: {
+					width: 'full',
+					interface: 'boolean',
+					options: {
+						label: 'Display a copy button next to the item',
+					},
+				},
+				schema: {
+					default_value: false,
+				},
+			},
+			// TODO: allow option only when "contentType" is one of "phone, url or email"
+			{
+				field: 'showLink',
+				name: 'Display link icon',
+				type: 'boolean',
+				meta: {
+					width: 'full',
+					interface: 'boolean',
+					options: {
+						label: 'Display a link button next to the item',
+					},
+				},
+				schema: {
+					default_value: false,
+				},
+			},
+		];
+
+		return options;
+	},
 });
