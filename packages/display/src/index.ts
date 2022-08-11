58d37b141cdc4998b1a9b52396fcf97b612cd54b
Dominic <domi.m.98@googlemail.com>
2022-08-11 20:35:38 +0200

    Refactor: move Display options into shared option.js

--- packages/display/src/index.ts
+++ packages/display/src/index.ts
@@ -1,6 +1,6 @@
 import { defineDisplay, } from '@directus/extensions-sdk';
-import { DisplayConfig } from '@directus/shared/types';
 import DisplayComponent from './display.vue';
+import { getSharedConfigOptions } from '../../../shared/options/sharedConfigOptions';
 
 export default defineDisplay({
 	id: 'custom',
@@ -12,126 +12,10 @@ export default defineDisplay({
 	options: ({ field  }): any => {
 		const isString = ['string', 'text'].includes(field.type ?? 'unknown');
 
-		const options: DisplayConfig['options'] = [
-			{
-				field: 'contentType',
-				name: 'Fields content type',
-				type: 'string',
-				meta: {
-					width: 'full',
-					interface: 'select-dropdown',
-					options: {
-						choices: getContentTypeChoices(isString),
-					}
-				},
-				schema: {
-					default_value: 'other',
-				},
-			},{
-				field: 'clickAction',
-				name: 'Click Action (when clicking on the value)',
-				type: 'string',
-				meta: {
-					width: 'full',
-					interface: 'select-dropdown',
-					options: {
-						choices: getClickActionChoices(isString),
-					}
-				},
-				schema: {
-					default_value: 'default',
-				},
-			},
-			{
-				field: 'showCopy',
-				name: 'Display copy icon',
-				type: 'boolean',
-				meta: {
-					width: 'full',
-					interface: 'boolean',
-					options: {
-						label: 'Display a copy button next to the item',
-					},
-				},
-				schema: {
-					default_value: false,
-				},
-			},
-			// TODO: allow option only when "contentType" is one of "phone, url or email" --> and set true in this cases
-			{
-				field: 'showLink',
-				name: 'Display link icon',
-				type: 'boolean',
-				meta: {
-					width: 'full',
-					interface: 'boolean',
-					options: {
-						label: 'Display a link button next to the item',
-					},
-				},
-				schema: {
-					default_value: false,
-				},
-			},
-		];
+		const options = getSharedConfigOptions(isString);
 
 		return options;
 	},
 });
 
 
-
-function getContentTypeChoices(isString: boolean) {
-	const selectChoices = [
-		{
-			text: 'Other',
-			value: 'other',
-		},
-		{
-			text: 'Phone',
-			value: 'phone',
-		},
-	];
-
-	if (isString) {
-		selectChoices.push(
-			{
-				text: 'URL',
-				value: 'url',
-			},
-			{
-				text: 'E-Mail',
-				value: 'email',
-			},
-		);
-	}
-
-	return selectChoices;
-}
-
-
-
-// dynamically build push Options
-// TODO: allow link only if phone, url or mail is selected
-function getClickActionChoices(isString: boolean) {
-	const selectChoices = [
-		{
-			text: 'Copy to clipboard',
-			value: 'copy',
-		},{
-			text: 'Open element (default)',
-			value: 'default',
-		},
-	];
-
-	if (isString) {
-		selectChoices.push(
-			{
-				text: 'Open link',
-				value: 'link',
-			},
-		);
-	}
-
-	return selectChoices;
-}
