aa54a3226480e3b871a3b3bd1ce43907c8bd214d
Dominic <domi.m.98@googlemail.com>
2022-08-23 18:35:19 +0200

    Interface: add a bunch of options

--- packages/interface/src/index.ts
+++ packages/interface/src/index.ts
@@ -1,6 +1,6 @@
 import { defineInterface } from '@directus/extensions-sdk';
 import InterfaceComponent from './interface.vue';
-import { getSharedConfigOptions } from '../../../shared/options/sharedConfigOptions';
+import { getSharedConfigOptions, getClickActionChoices } from '../../../shared/options/sharedConfigOptions';
 
 export default defineInterface({
 	id: 'custom',
@@ -11,9 +11,107 @@ export default defineInterface({
 	types: ['string', 'text', 'bigInteger', 'integer', 'decimal', 'float'],
 	options: ({ field  }): any => {
 		const isString = ['string', 'text'].includes(field.type ?? 'unknown');
+		
 
-		const options = getSharedConfigOptions(isString);
+		const sharedOptions = getSharedConfigOptions(isString);
+		// TODO: add custom options:  iconLeft, iconRight, softLength, clear, font
+		
+		const interfaceOptions = [
+			{
+				field: 'placeholder',
+				name: '$t:placeholder',
+				meta: {
+					width: 'half',
+					interface: 'system-input-translated-string',
+					options: {
+						placeholder: '$t:enter_a_placeholder',
+					},
+				},
+			},
+			{
+				field: 'iconLeft',
+				name: '$t:icon_left',
+				type: 'string',
+				meta: {
+					width: 'half',
+					interface: 'select-icon',
+				},
+			},
+			{
+				field: 'iconRight',
+				name: '$t:icon_right',
+				type: 'string',
+				meta: {
+					width: 'half',
+					interface: 'select-icon',
+				},
+			},
+		];
 
-		return options;
+
+		const readOnlyOptions = [
+			{
+				field: 'clickAction',
+				name: 'Click Action (when clicking on the value)',
+				type: 'string',
+				meta: {
+					width: 'full',
+					interface: 'select-dropdown',
+					options: {
+						choices: getClickActionChoices(isString),
+					}
+				},
+				schema: {
+					default_value: 'default',
+				},
+			},
+		];
+
+		const numberOptions = [
+			{
+				field: 'min',
+				name: '$t:interfaces.input.minimum_value',
+				type: 'integer',
+				meta: {
+					width: 'half',
+					interface: 'input',
+				},
+			},
+			{
+				field: 'max',
+				name: '$t:interfaces.input.maximum_value',
+				type: 'integer',
+				meta: {
+					width: 'half',
+					interface: 'input',
+				},
+			},
+			{
+				field: 'step',
+				name: '$t:interfaces.input.step_interval',
+				type: 'integer',
+				meta: {
+					width: 'half',
+					interface: 'input',
+				},
+				schema: {
+					default_value: 1,
+				},
+			}
+		];
+
+
+		if (field.type && ['bigInteger', 'integer', 'float', 'decimal'].includes(field.type)) {
+			const allOptions = [...interfaceOptions, ...numberOptions, ...sharedOptions ];
+			if (field.meta?.readonly) allOptions.push(...readOnlyOptions);
+			
+			return allOptions;
+		}
+	
+
+		const allOptions = [...interfaceOptions, ...sharedOptions];
+		if (field.meta?.readonly) allOptions.push(...readOnlyOptions);
+		
+		return allOptions;
 	},
 });
--- packages/interface/src/interface.vue
+++ packages/interface/src/interface.vue
@@ -1,27 +1,131 @@
+<!-- @see https://github.com/directus/directus/blob/main/app/src/interfaces/input/input.vue -->
+
 <template>
-	<input :value="value" @input="handleChange($event.target.value)" />
+	<div class="action-interface">
+		<v-input 
+			:model-value="value" 
+			:disabled="disabled"
+			:type="inputType"
+			@update:model-value="$emit('input', $event)"
+		>
+			<template v-if="iconLeft" #prepend><v-icon :name="iconLeft" /></template>
+		</v-input>
+
+		<!-- TODO: add tooltips -->
+		<v-icon 
+			v-if="showCopy && isCopySupported"
+			name="content_copy"
+			right
+			@click.stop="copyValue"
+			v-tooltip="`Copy to clipboard: ${value}`"
+		/>
+		
+
+		<a 
+			v-if="showLink"
+			:href="computedLink"
+			target="_blank"
+			rel="noopener noreferrer"
+			@click.stop
+		>
+			<v-icon 
+				name="open_in_new"
+				right
+				v-tooltip="`Follow link: ${computedLink}`"
+			/>
+		</a>
+	</div>
+	
 	<!-- TODO: create interface -->
 	<!-- TODO: share button stuff with display -->
 	<!-- TODO: add input for different types, add readonly support -->
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useClipboard } from '../../../shared/composable/use-clipboard';
+import { useLink } from '../../../shared/composable/use-link';
+import { useStores } from '@directus/extensions-sdk';
 
-export default defineComponent({
-	props: {
-		value: {
-			type: String,
-			default: null,
-		},
+const props = defineProps({
+	value: {
+		type: [String, Number],
+		default: null,
 	},
-	emits: ['input'],
-	setup(props, { emit }) {
-		return { handleChange };
-
-		function handleChange(value: string): void {
-			emit('input', value);
-		}
+	contentType: { // TODO: use content type for action
+		type: String,
+		default: 'other',
+	},
+	clickAction: {	// TODO add action
+		type: String,
+		default: 'default',
+	},
+	showCopy: {
+		type: Boolean,
+		default: false,
+	},
+	showLink: {
+		type: Boolean,
+		default: false,
+	},
+	placeholder: {
+		type: String,
+		default: null,
+	},
+	iconLeft: {
+		type: String,
+		default: null,
+	},
+	iconRight: {
+		type: String,
+		default: null,
+	},
+	min: {
+		type: Number,
+		default: undefined,
+	},
+	max: {
+		type: Number,
+		default: undefined,
+	},
+	step: {
+		type: Number,
+		default: 1,
+	},
+	// global optiopns (independend from this interface)
+	type: {
+		type: String,
+		default: null,
+	},
+	disabled: {
+		type: Boolean,
+		default: false,
 	},
 });
+
+const inputType = computed(() => {
+	if (['bigInteger', 'integer', 'float', 'decimal'].includes(props.type)) return 'number';
+	return 'text';
+});
+
+const { isCopySupported, copyToClipboard } = useClipboard();
+
+const { useNotificationsStore } = useStores();
+const notificationStore = useNotificationsStore();	
+
+async function copyValue() {
+	await copyToClipboard( (inputType.value === 'number') ? props.value.toString() : props.value as string, notificationStore);
+};
+
+
+const { computedLink } = useLink(props);
+
 </script>
+
+
+<style scoped lang="scss">
+.action-interface {
+	display: flex;
+	flex-direction: row;
+}
+</style>
