fc09e919c000b7e5fb5291037780bd4b26ce9139
Dominic <dominic.marcelino@utomic.media>
2024-03-06 14:39:35 +0100

    FIX: interface default click action

--- src/interface/interface.vue
+++ src/interface/interface.vue
@@ -192,6 +192,20 @@ const actionTooltip = computed(() => {
 
 
 
+<style lang="scss">
+	// !NOTE: GLOBAL STYLES - use scoped styles for the component whenever possible!
+	.action-interface {
+		.v-input {
+			input:disabled {
+				// disable click events on disabled inputs, so that the click event can be handled by the parent div
+				// For some reason we can't go with a normal :deep() selector in the scoped stye
+				pointer-events: none;
+			}
+		}
+	}
+</style>
+
+
 <style scoped lang="scss">
 .action-interface {
 	display: flex;
